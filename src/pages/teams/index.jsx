e15f75116c140dede4c03726429f40dd5d3cf58a
MaksimLogvinenko <maksimlogvinenko98@gmail.com>
2022-11-21 16:10:31 +0200

    added search team input

--- src/pages/teams/index.jsx
+++ src/pages/teams/index.jsx
@@ -31,6 +31,7 @@ const Teams = () => {
   const [showSearch, setShowSearch] = React.useState(false);
   const [showTeamInfo, setShowTeamInfo] = React.useState(false);
   const [showEdit, setShowEdit] = React.useState(false);
+  const [serchValue, setSearchValue] = React.useState("");
 
   const handleClickOutside = () => {
     setShowEdit(false);
@@ -103,6 +104,8 @@ const Teams = () => {
                 } bg-blackSecond h-full text-xs text-gray border-none placeholder:text-gray duration-300 rounded-r w-full focus:shadow-none focus:ring-0`}
                 type="search"
                 placeholder="Enter user name to search..."
+                value={serchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
               />
             </div>
           </div>
@@ -121,90 +124,95 @@ const Teams = () => {
         <h3 className="h3 mb-5">Teams ({dataGetTeams.length})</h3>
         <div className="flex flex-col gap-4">
           {dataGetTeams &&
-            dataGetTeams.map((team, index) => (
-              <div
-                key={team.id}
-                className="flex flex-col relative bg-blackSecond px-4 py-2 rounded"
-              >
+            dataGetTeams
+              .filter((el) =>
+                el.name.toLowerCase().includes(serchValue.toLowerCase().trim())
+              )
+              .map((team, index) => (
                 <div
-                  ref={refPopup}
-                  className={`${
-                    showEdit === index
-                      ? "opacity-100 visible"
-                      : "opacity-0 invisible"
-                  } duration-300 absolute top-12 z-10 right-1 bg-backGround shadow lg:top-16`}
+                  key={team.id}
+                  className="flex flex-col relative bg-blackSecond px-4 py-2 rounded"
                 >
-                  <Link
-                    className="px-7 py-2 block text-gray duration-300 cursor-pointer hover:bg-blackSecond hover:text-primary"
-                    to="/teams"
+                  <div
+                    ref={refPopup}
+                    className={`${
+                      showEdit === index
+                        ? "opacity-100 visible"
+                        : "opacity-0 invisible"
+                    } duration-300 absolute top-12 z-10 right-1 bg-backGround shadow lg:top-16`}
                   >
-                    Granting privileges
-                  </Link>
-                  {getPermissionsUserId && getPermissionsUserId.length !== 0 ? (
+                    <Link
+                      className="px-7 py-2 block text-gray duration-300 cursor-pointer hover:bg-blackSecond hover:text-primary"
+                      to="/teams"
+                    >
+                      Granting privileges
+                    </Link>
+                    {getPermissionsUserId &&
+                    getPermissionsUserId.length !== 0 ? (
+                      <div
+                        onClick={(e) => navigate(`/teams/${team.id}`)}
+                        className="px-7 py-2 block text-gray duration-300 cursor-pointer hover:bg-blackSecond hover:text-primary"
+                      >
+                        Editing
+                      </div>
+                    ) : (
+                      false
+                    )}
                     <div
-                      onClick={(e) => navigate(`/teams/${team.id}`)}
+                      onClick={() => handleDeleteTeam(team.id)}
                       className="px-7 py-2 block text-gray duration-300 cursor-pointer hover:bg-blackSecond hover:text-primary"
                     >
-                      Editing
+                      Remove
                     </div>
-                  ) : (
-                    false
+                  </div>
+                  <div className="flex justify-between items-center pb-2 border-b border-backGround">
+                    <div className="flex flex-col">
+                      <h5 className="text-gray text-xs">
+                        Name of the department:
+                      </h5>
+                      <h4>{team.name}</h4>
+                    </div>
+                    <BiDotsVerticalRounded
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        toggleEdit(index);
+                      }}
+                      className="text-3xl text-gray cursor-pointer"
+                    />
+                  </div>
+                  {showTeamInfo === index && (
+                    <div className={`duration-300 flex flex-col pt-4`}>
+                      <p className="pb-4">Members of the department:</p>
+                      <div className="flex">
+                        {!loadingGetTeams && team.users.length ? (
+                          team.users.map((item) => (
+                            <img
+                              key={item.id}
+                              className="w-[29px] h-[29px] shadow -ml-1"
+                              src={UserImg}
+                              alt=""
+                            />
+                          ))
+                        ) : (
+                          <p className="text-danger">NO USERS</p>
+                        )}
+                      </div>
+                    </div>
                   )}
                   <div
-                    onClick={() => handleDeleteTeam(team.id)}
-                    className="px-7 py-2 block text-gray duration-300 cursor-pointer hover:bg-blackSecond hover:text-primary"
-                  >
-                    Remove
-                  </div>
-                </div>
-                <div className="flex justify-between items-center pb-2 border-b border-backGround">
-                  <div className="flex flex-col">
-                    <h5 className="text-gray text-xs">
-                      Name of the department:
-                    </h5>
-                    <h4>{team.name}</h4>
-                  </div>
-                  <BiDotsVerticalRounded
+                    className="flex justify-center py-2"
                     onClick={(e) => {
-                      e.stopPropagation();
-                      toggleEdit(index);
+                      toggle(index);
                     }}
-                    className="text-3xl text-gray cursor-pointer"
-                  />
-                </div>
-                {showTeamInfo === index && (
-                  <div className={`duration-300 flex flex-col pt-4`}>
-                    <p className="pb-4">Members of the department:</p>
-                    <div className="flex">
-                      {!loadingGetTeams && team.users.length ? (
-                        team.users.map((item) => (
-                          <img
-                            key={item.id}
-                            className="w-[29px] h-[29px] shadow -ml-1"
-                            src={UserImg}
-                            alt=""
-                          />
-                        ))
-                      ) : (
-                        <p className="text-danger">NO USERS</p>
-                      )}
-                    </div>
+                  >
+                    <IoIosArrowDown
+                      className={`${
+                        showTeamInfo === index ? "rotate-180" : "rotate-0"
+                      } duration-300 text-gray text-base`}
+                    />
                   </div>
-                )}
-                <div
-                  className="flex justify-center py-2"
-                  onClick={(e) => {
-                    toggle(index);
-                  }}
-                >
-                  <IoIosArrowDown
-                    className={`${
-                      showTeamInfo === index ? "rotate-180" : "rotate-0"
-                    } duration-300 text-gray text-base`}
-                  />
                 </div>
-              </div>
-            ))}
+              ))}
         </div>
       </div>
     </div>
