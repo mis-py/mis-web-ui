f75be0db31ccd8efb1a5c82996ffb340f409d8c3
MaksimLogvinenko <maksimlogvinenko98@gmail.com>
2023-01-25 13:23:20 +0200

    added new user settings (test)

--- src/pages/users/AddUserSettings.jsx
+++ src/pages/users/AddUserSettings.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useGetUserSettingsQuery } from "../../redux";
 
 import { IoIosArrowBack } from "react-icons/io";
@@ -7,22 +7,21 @@ import { FiSearch } from "react-icons/fi";
 
 const AddUserSettings = () => {
   const navigate = useNavigate();
-  const [searchInput, setSearchInput] = React.useState("");
   const { data: getUserSettings } = useGetUserSettingsQuery();
 
-  console.log(getUserSettings && getUserSettings);
+  const [searchValue, setSearchValue] = React.useState("");
 
   return (
     <div className="py-6 min-h-screen h-full flex flex-col justify-between">
       <div className="flex flex-col">
-        <div className="flex items-center text-gray">
+        <Link to={-1} className="flex items-center text-gray">
           <div className="flex mr-2">
             <IoIosArrowBack />
           </div>
-          <div onClick={() => navigate(-1)}>back</div>
-        </div>
+          <span>back</span>
+        </Link>
         <h3 className="h3 mt-5">Settings</h3>
-        <form className="my-4">
+        <form className="my-4 pb-[50px]">
           <label
             className="flex justify-between items-center bg-blackSecond rounded text-sm text-gray mb-7"
             htmlFor="search"
@@ -31,31 +30,28 @@ const AddUserSettings = () => {
               className="w-full bg-transparent border-none focus:shadow-none focus:ring-0"
               type="search"
               placeholder="Enter setting name to search..."
-              value={searchInput}
-              onChange={(e) => setSearchInput(e.target.value)}
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
             />
             <FiSearch className="w-12 text-gray" />
           </label>
 
           {getUserSettings
-            ?.filter((set) =>
-              set.app.name
-                .toLowerCase()
-                .includes(searchInput.toLowerCase().trim())
+            ?.filter((el) =>
+              el.key.toLowerCase().includes(searchValue.toLowerCase().trim())
             )
-            .map((setting) => (
+            .map((item, index) => (
               <label
-                key={setting.id}
+                key={item.id}
                 className="flex flex-col gap-1 mb-4"
-                htmlFor={setting.key}
+                htmlFor={item.key}
               >
-                {setting.app.name}
+                {item.key}
                 <input
-                  type={setting.type}
-                  name={setting.key}
-                  id={setting.key}
-                  //   checked={checkSuperUser}
-                  //   onChange={() => setCheckSuperUser(!checkSuperUser)}
+                  autoComplete="off"
+                  type={item.type}
+                  name={item.key}
+                  id={item.key}
                   className="bg-blackSecond text-gray rounded px-3 py-2 focus-visible:outline-none border-none"
                 />
               </label>
