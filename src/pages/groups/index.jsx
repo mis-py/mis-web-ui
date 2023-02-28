905f0c4bc7cf973f08e90d0ecf1445fa1b6672b1
MaksimLogvinenko <maksimlogvinenko98@gmail.com>
2023-02-28 12:05:46 +0200

    fix design group card

--- src/pages/groups/index.jsx
+++ src/pages/groups/index.jsx
@@ -2,26 +2,18 @@ import React from "react";
 import { toast } from "react-toastify";
 import PulseLoader from "react-spinners/PulseLoader";
 import { confirmAlert } from "react-confirm-alert";
-import {
-  useGetGroupsQuery,
-  useGetPermissionsUserIdQuery,
-  useDeleteGroupMutation,
-} from "../../redux";
-import { deleteMembersAll } from "../../redux/slices/membersSlice";
+import { useGetGroupsQuery, useDeleteGroupMutation } from "redux/index";
+import { deleteMembersAll } from "redux/slices/membersSlice";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import useOutsideClick from "../../hooks/useOutsideClick";
+import useOutsideClick from "hooks/useOutsideClick";
 
-import Tooltip from "../../components/Tooltip";
-import AdminWrapper from "../../config/AdminWrapper";
+import Tooltip from "components/Tooltip";
+import AdminWrapper from "config/AdminWrapper";
 
 import { FiSearch } from "react-icons/fi";
 import { BiDotsVerticalRounded } from "react-icons/bi";
-import { IoIosArrowDown } from "react-icons/io";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
-
-import UserImg from "../../assets/img/user.png";
-import GroupImg from "../../assets/img/groups.png";
 
 const Groups = () => {
   const navigate = useNavigate();
@@ -31,13 +23,11 @@ const Groups = () => {
     isLoading: loadingGroup,
     error: errorGroup,
   } = useGetGroupsQuery();
-
   const [deleteGroup] = useDeleteGroupMutation();
 
-  const [showSearch, setShowSearch] = React.useState(false);
-  const [showTeamInfo, setShowTeamInfo] = React.useState(false);
-  const [showEdit, setShowEdit] = React.useState(false);
   const [serchValue, setSearchValue] = React.useState("");
+  const [showSearch, setShowSearch] = React.useState(false);
+  const [showEdit, setShowEdit] = React.useState(false);
 
   const handleClickOutside = () => {
     setShowEdit(false);
@@ -51,14 +41,6 @@ const Groups = () => {
     }
     dispatch(deleteMembersAll());
   }, [errorGroup, loadingGroup]);
-
-  const toggle = (index) => {
-    if (showTeamInfo === index) {
-      setShowTeamInfo(false);
-      return;
-    }
-    setShowTeamInfo(index);
-  };
 
   const toggleEdit = (index) => {
     if (showEdit === index) {
@@ -126,7 +108,7 @@ const Groups = () => {
           </AdminWrapper>
         </div>
 
-        <h3 className="h3 mb-5">Groups ({getGroups.length})</h3>
+        <h3 className="h3 mb-5">Groups ({getGroups?.length})</h3>
         {loadingGroup ? (
           <PulseLoader
             size={15}
@@ -139,109 +121,92 @@ const Groups = () => {
           />
         ) : (
           <div className="flex flex-col gap-4">
-            {getGroups &&
-              getGroups
-                .filter((el) =>
-                  el.name
-                    .toLowerCase()
-                    .includes(serchValue.toLowerCase().trim())
-                )
-                .map((group, index) => (
+            {getGroups
+              ?.filter((el) =>
+                el.name.toLowerCase().includes(serchValue.toLowerCase().trim())
+              )
+              .map((group, index) => (
+                <div
+                  key={group.id}
+                  className="flex flex-col relative bg-blackSecond p-6 rounded lg:pt-6 lg:px-6"
+                >
                   <div
-                    key={group.id}
-                    className="flex flex-col relative bg-blackSecond px-4 py-2 rounded lg:pt-6 lg:px-6"
+                    ref={refPopup}
+                    className={`${
+                      showEdit === index
+                        ? "opacity-100 visible"
+                        : "opacity-0 invisible"
+                    } duration-300 absolute top-12 z-10 right-1 bg-backGround shadow lg:top-3`}
                   >
                     <div
-                      ref={refPopup}
-                      className={`${
-                        showEdit === index
-                          ? "opacity-100 visible"
-                          : "opacity-0 invisible"
-                      } duration-300 absolute top-12 z-10 right-1 bg-backGround shadow lg:top-3`}
+                      onClick={() => navigate(`/group/members/${group.id}`)}
+                      className="px-7 py-2 block text-gray duration-300 cursor-pointer hover:bg-blackSecond hover:text-primary"
                     >
-                      <div
-                        onClick={(e) => navigate(`/group/members/${group.id}`)}
-                        className="px-7 py-2 block text-gray duration-300 cursor-pointer hover:bg-blackSecond hover:text-primary"
-                      >
-                        Manage members
-                      </div>
-
-                      <div
-                        onClick={(e) => navigate(`/group/objects/${group.id}`)}
-                        className="px-7 py-2 block text-gray duration-300 cursor-pointer hover:bg-blackSecond hover:text-primary"
-                      >
-                        Manage objects
-                      </div>
-
-                      <div
-                        onClick={() => handleDeleteGroup(group.id)}
-                        className="px-7 py-2 block text-gray duration-300 cursor-pointer hover:bg-blackSecond hover:text-primary"
-                      >
-                        Remove
-                      </div>
+                      Manage members
                     </div>
-                    <div className="flex justify-between items-center pb-2 border-b border-backGround">
-                      <div className="flex items-center gap-4">
-                        <img
-                          className="w-[56px] h-[56px]"
-                          src={GroupImg}
-                          alt=""
-                        />
-                        <div className="flex flex-col">
-                          <h5 className="text-gray text-xs">
-                            Name of the department:
-                          </h5>
-                          <h4>{group.name}</h4>
-                        </div>
-                      </div>
-                      <AdminWrapper>
-                        <BiDotsVerticalRounded
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            toggleEdit(index);
-                          }}
-                          className="text-3xl text-gray cursor-pointer"
-                        />
-                      </AdminWrapper>
-                    </div>
-                    {showTeamInfo === index && (
-                      <div className={`duration-300 flex flex-col pt-4`}>
-                        <p className="pb-4">Members of the department:</p>
-                        <div className="flex">
-                          {!loadingGroup && group.users.length ? (
-                            group.users.map((item) => (
-                              <div
-                                key={item.id}
-                                className="group cursor-pointer shadow -ml-1 relative"
-                              >
-                                <img
-                                  className="w-[35px] h-[35px]"
-                                  src={UserImg}
-                                  alt=""
-                                />
-                                <Tooltip name={item.username} />
-                              </div>
-                            ))
-                          ) : (
-                            <p className="text-danger">NO USERS</p>
-                          )}
-                        </div>
-                      </div>
-                    )}
+
                     <div
-                      className="flex justify-center py-2 cursor-pointer"
-                      onClick={(e) => {
-                        toggle(index);
-                      }}
+                      onClick={() => navigate(`/group/objects/${group.id}`)}
+                      className="px-7 py-2 block text-gray duration-300 cursor-pointer hover:bg-blackSecond hover:text-primary"
                     >
-                      <IoIosArrowDown
-                        className={`${
-                          showTeamInfo === index ? "rotate-180" : "rotate-0"
-                        } duration-300 text-gray text-base`}
-                      />
+                      Manage objects
+                    </div>
+
+                    <div
+                      onClick={() => handleDeleteGroup(group.id)}
+                      className="px-7 py-2 block text-gray duration-300 cursor-pointer hover:bg-blackSecond hover:text-primary"
+                    >
+                      Remove
                     </div>
                   </div>
-                ))}
+                  <div className="flex justify-between items-center pb-2 border-b border-backGround">
+                    <div className="flex items-center gap-4">
+                      <img
+                        className="w-[56px] h-[56px]"
+                        src={require("assets/img/groups.png")}
+                        alt=""
+                      />
+                      <div className="flex flex-col">
+                        <h5 className="text-gray text-xs">
+                          Name of the department:
+                        </h5>
+                        <h4>{group.name}</h4>
+                      </div>
+                    </div>
+                    <AdminWrapper>
+                      <BiDotsVerticalRounded
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          toggleEdit(index);
+                        }}
+                        className="text-3xl text-gray cursor-pointer"
+                      />
+                    </AdminWrapper>
+                  </div>
+                  <div className={`duration-300 flex flex-col pt-4`}>
+                    <p className="pb-4">Members of the department:</p>
+                    <div className="flex pl-1">
+                      {group.users.length ? (
+                        group.users.map((item) => (
+                          <div
+                            key={item.id}
+                            className="group cursor-pointer shadow -ml-1 relative"
+                          >
+                            <img
+                              className="w-[35px] h-[35px]"
+                              src={require("assets/img/user.png")}
+                              alt=""
+                            />
+                            <Tooltip name={item.username} />
+                          </div>
+                        ))
+                      ) : (
+                        <p className="text-danger">NO USERS</p>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              ))}
           </div>
         )}
       </div>
