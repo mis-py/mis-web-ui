493d8c647a6804b5c56ab68f4f2bee4106398ad8
MaksimLogvinenko <maksimlogvinenko98@gmail.com>
2023-03-22 11:33:38 +0200

    fix task api

--- src/redux/api/modulesApi/timerApi.js
+++ src/redux/api/modulesApi/timerApi.js
@@ -2,27 +2,25 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { baseUrl } from "config/variables";
 
 export const timerApi = createApi({
-    reducerPath: "timerApi",
-    tagTypes: ["Timer"],
-    baseQuery: fetchBaseQuery({
-        baseUrl,
+  reducerPath: "timerApi",
+  tagTypes: ["Timer"],
+  baseQuery: fetchBaseQuery({
+    baseUrl,
+  }),
+  endpoints: (build) => ({
+    timerLead: build.mutation({
+      query: (body) => ({
+        url: `/timer/lead`,
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${localStorage.getItem("my-token")}`,
+        },
+        body,
+      }),
+      invalidatesTags: [{ type: "Timer", id: "LIST" }],
     }),
-    endpoints: (build) => ({
-        timerLead: build.mutation({
-            query: () => ({
-                url: `/timer/lead`,
-                method: "POST",
-                headers: {
-                    accept: "application/json",
-                    Authorization: `Bearer ${localStorage.getItem("my-token")}`,
-                },
-            }),
-            invalidatesTags: [{ type: "Timer", id: "LIST" }],  
-        })
-    }),
-
+  }),
 });
 
-export const {
-    useTimerLeadMutation
-} = timerApi;
+export const { useTimerLeadMutation } = timerApi;
