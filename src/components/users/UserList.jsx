1adf6f75a02c6ce69592a44d14314174448b4e95
MaksimLogvinenko <maksimlogvinenko98@gmail.com>
2023-06-23 13:21:19 +0300

    added components user list, user item

---
+++ src/components/users/UserList.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import PulseLoader from "react-spinners/PulseLoader";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { resetUser } from "redux/slices/userSlice";
+
+import UserItem from "./UserItem";
+
+const UserList = ({ searchValue, getUsers, loadingGetUser, errorGetUsers }) => {
+  const dispatch = useDispatch();
+  const [showEdit, setShowEdit] = React.useState(false);
+
+  React.useEffect(() => {
+    dispatch(resetUser());
+    if (errorGetUsers) {
+      toast.error("No users found");
+    }
+  }, [errorGetUsers, searchValue]);
+
+  return loadingGetUser ? (
+    <PulseLoader
+      size={15}
+      cssOverride={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+      color="#757575"
+    />
+  ) : (
+    <div className="flex flex-col gap-4">
+      {getUsers
+        ?.filter((el) =>
+          el.username.toLowerCase().includes(searchValue.toLowerCase().trim())
+        )
+        .map((user, index) => (
+          <UserItem
+            key={user.id}
+            user={user}
+            index={index}
+            showEdit={showEdit}
+            setShowEdit={setShowEdit}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default UserList;
