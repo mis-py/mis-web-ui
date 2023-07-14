d6ac46b94de32d037f4611c26034e9ce99693d04
dashalandau <dashalandau4@gmail.com>
2023-07-14 16:00:00 +0300

    Added basic App logs realisation

--- src/pages/apps/LogsApp.jsx
+++ src/pages/apps/LogsApp.jsx
@@ -1,48 +1,39 @@
 import React from "react";
-import { useNavigate, Link } from "react-router-dom";
-import Terminal, { ColorMode, TerminalOutput } from "react-terminal-ui";
+import PageHeader from "../../components/common/PageHeader";
 
-import { IoIosArrowBack } from "react-icons/io";
-import { FiSearch } from "react-icons/fi";
+import { useGetAppLogsQuery } from "redux/index";
+
+import {useParams} from "react-router-dom";
 
 const LogsApp = () => {
-  const navigate = useNavigate();
-  const [terminalLineData, setTerminalLineData] = React.useState([
-    // <TerminalOutput>Welcome to the App name logs</TerminalOutput>,
-  ]);
+
+  const { id } = useParams();
+  const { data: appLogs, refetch: refetchLogs, isLoading: isLogsLoading } = useGetAppLogsQuery({ id });
+
+  const [terminalValue, setTerminalValue] = React.useState("");
+
+  React.useEffect(() => {
+    setTerminalValue(appLogs);
+  }, [isLogsLoading]);
 
   return (
     <div className="py-6 min-h-screen h-full flex flex-col justify-between">
       <div className="flex flex-col">
-        <Link to={-1} className="flex items-center text-gray">
-          <div className="flex mr-2">
-            <IoIosArrowBack />
-          </div>
-          <span>back</span>
-        </Link>
-        <h3 className="h3 mt-5">App name logs</h3>
-        <form className="my-4">
-          <label
-            className="flex justify-between items-center bg-blackSecond rounded text-sm text-gray mb-7"
-            htmlFor="search"
-          >
-            <input
-              className="w-full bg-transparent border-none focus:shadow-none focus:ring-0"
-              type="search"
-              placeholder="Search..."
-            />
-            <FiSearch className="w-12 text-gray" />
-          </label>
-        </form>
-        <Terminal
-          // name="React Terminal Usage Example"
-          colorMode={ColorMode.Dark}
-          onInput={(terminalInput) =>
-            console.log(`New terminal input received: '${terminalInput}'`)
-          }
-        >
-          {terminalLineData}
-        </Terminal>
+        <PageHeader
+          header={`App name logs`}
+        />
+
+        <pre>
+          {terminalValue}
+        </pre>
+
+        {/*<ReactTerminal*/}
+        {/*    commands={{}}*/}
+        {/*    enableInput={false}*/}
+        {/*    showControlBar={false}*/}
+        {/*    prompt={terminalValue}*/}
+        {/*    theme="dark"*/}
+        {/*/>*/}
       </div>
     </div>
   );
