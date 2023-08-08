f8805510092ce47499b8f166ee0f0c23b3131cd4
dashalandau <dashalandau4@gmail.com>
2023-08-08 13:09:47 +0300

    Remove websocket connection

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ import { taskRoutes } from "routes/tasks";
 import { useSelector } from 'react-redux';
 import LoadingOverlay from "./components/common/LoadingOverlay";
 import { consumersRoutes } from "routes/consumers";
-import webSocket from "./config/WebSocketConnection";
 
 function App() {
   const navigate = useNavigate();
@@ -37,10 +36,6 @@ function App() {
             }
         }
     }, [location.pathname, navigate]);
-
-    webSocket.onopen = function (e) {
-        webSocket.send('{"subscribe": "notifications"}');
-    }
 
   const isLoading = useSelector(state => state.loading);
   const modulesList = useModuleRoutes();
