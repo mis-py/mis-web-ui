9b2c512e0b86c4b2c878deb0239fef047ff65dc7
dashalandau <dashalandau4@gmail.com>
2023-08-11 10:55:12 +0300

    Print notifications in "Notifications" component

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { initiateWebSocket } from "config/WebSocketConnection";
 
 function Notifications({isPopupOpen, setIsPopupOpen, notificationsCount, setNotificationsCount}){
-    const notifications =[
-        {id: 1, text: 'Уведомление 1'},
-        {id: 2, text: 'Уведомление 2'},
-        {id: 3, text: 'Уведомление 3'},
-    ];
+    const [notifications, setNotifications] = React.useState([]);
+    const webSocket = initiateWebSocket();
+
+    if (webSocket !== undefined) {
+        webSocket.onmessage = function (message) {
+            const _notifications = notifications;
+            const data = JSON.parse(message.data);
+
+            if (data.action !== "notifications") {
+                return
+            }
+
+            _notifications.push({
+                header: data.data.key_verbose || data.data.key || null,
+                body: data.data.body_verbose || JSON.stringify(data.data.message.body),
+                date: data.time,
+            });
+
+            setNotifications(_notifications);
+            setNotificationsCount(_notifications.length);
+        }
+    }
 
     const handleButtonClick = () => {
         setIsPopupOpen(!isPopupOpen);
@@ -13,20 +31,24 @@ function Notifications({isPopupOpen, setIsPopupOpen, notificationsCount, setNoti
     };
 
     return (
-        <div>
-          <button onClick={handleButtonClick}>
-            Уведомления {notificationsCount > 0 && `(${notificationsCount})`}
-          </button>
-          {isPopupOpen && (
-            <div>
+        <div className="w-[220px]">
+            <div className="mt-2 px-5 pb-4 text-xl font-semibold" onClick={handleButtonClick}>
+              Notifications{notificationsCount > 0 && ` (${notificationsCount})`}
+            </div>
+            <div className="max-h-[330px] overflow-auto">
               <ul>
-                {notifications.map(notification => (
-                  <li key={notification.id}>{notification.text}</li>
+                {notifications.map((notification, index) => (
+                  <li
+                      className={`px-5 ${index + 1 !== notifications.length ? "pb-2" : ""}`}
+                      key={notification.date + notification.header}
+                  >
+                      <div className="pb-1 leading-none font-medium">{notification.header}</div>
+                      <div>{notification.body}</div>
+                  </li>
                 ))}
               </ul>
-              <button onClick={handleButtonClick}>Закрыть</button>
             </div>
-          )}
+            <button className="btn-primary mt-4 w-full" onClick={handleButtonClick}>Закрыть</button>
         </div>
     )
 }
